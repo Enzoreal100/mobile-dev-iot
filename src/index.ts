bad58f02fb6c0fe86dc05ca55c269900c15b9f9f
Enzoreal100 <enzoreal100@gmail.com>
2026-02-27 20:53:20 -0300

    feat: created exibir consulta

--- src/index.ts
+++ src/index.ts
@@ -25,3 +25,19 @@ function cancelarConsulta(consulta: Consulta): Consulta | null {
     status: "cancelada",
   };
 }
+
+function exibirConsulta(consulta: Consulta): string {
+  const valorFormatado = consulta.valor.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+  return `
+    Consulta #${consulta.id}
+    Médico: ${consulta.medico.nome}
+    Paciente: ${consulta.paciente.nome}
+    Especialidade: ${consulta.medico.especialidade.nome}
+    Data: ${consulta.data.toLocaleDateString("pt-BR")}
+    Valor: ${valorFormatado}
+    Status: ${consulta.status}
+    `;
+}
