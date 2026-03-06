bd2e78063371f8b46ee5394a22b57db1b9ae9b3d
Enzoreal100 <enzoreal100@gmail.com>
2026-03-05 22:21:00 -0300

    feat: added button for count function (remembering how to use useState :) )

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [clicks, getClicks] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title="Press me" onPress={() => getClicks(clicks + 1)} />
+      <Text>You have pressed the button {clicks} times.</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 20,
   },
 });
