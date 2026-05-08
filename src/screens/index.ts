365290bfe1596bfee14171b3bd88c922fdbb2f9d
enzoreal100 <enzoreal100@gmail.com>
2026-05-07 22:08:02 -0300

    refac: changed homescreen import

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,5 +1,4 @@
-export { default as Home } from "./Home";
-export { default as Admin } from "./Admin";
+export { default as HomeScreen } from "./HomeScreen";
 export { default as ConsultasListScreen } from "./ConsultasListScreen";
 export { default as ConsultaDetalhesScreen } from "./ConsultaDetalhesScreen";
 export { default as NovaConsultaScreen } from "./NovaConsultaScreen";
