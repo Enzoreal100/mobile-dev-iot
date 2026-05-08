059131736f372e4f35301659ca5c3fb18667ea55
enzoreal100 <enzoreal100@gmail.com>
2026-05-07 21:46:39 -0300

    feat: added usuarios.ts

---
+++ src/types/usuarios.ts
@@ -0,0 +1,16 @@
+export type TipoUsuario = "paciente" | "admin";
+ 
+export interface Usuario {
+ id: number;
+ nome: string;
+ email: string;
+ senha?: string;
+ cpf: string;
+ telefone: string;
+ perfil: TipoUsuario;
+}
+
+export const ADMIN_CREDENTIALS = {
+ email: "admin@sistema.com",
+ senha: "admin123",
+};
