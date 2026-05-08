0bea5c196c3975eca52fdc45d372457dabf99ee3
enzoreal100 <enzoreal100@gmail.com>
2026-05-07 22:05:46 -0300

    feat: implemented auth context

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,2 +1,10 @@
 export { default as Home } from "./Home";
 export { default as Admin } from "./Admin";
+export { default as ConsultasListScreen } from "./ConsultasListScreen";
+export { default as ConsultaDetalhesScreen } from "./ConsultaDetalhesScreen";
+export { default as NovaConsultaScreen } from "./NovaConsultaScreen";
+export { default as LoginScreen } from "./Login";
+export { default as CadastroPacienteScreen } from "./CadastroPaciente";
+export { default as MinhasConsultasScreen } from "./MinhasConsultas";
+export { default as AdminScreen } from "./Admin";
+export { default as AgendamentoScreen } from "./Agendamento";
