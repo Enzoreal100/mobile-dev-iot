43f684cca198c41c9336e11c9e9a9d26f05e156a
Enzoreal100 <enzoreal100@gmail.com>
2026-08-20 20:08:42 -0300

    feat: created blood pressure screen

--- src/interfaces/consulta.ts
+++ src/interfaces/consulta.ts
@@ -1,16 +1,22 @@
 import { StatusConsulta } from "../types/statusConsulta";
 import { Especialidade } from "../types/especialidade";
+import { ClassificacaoPA } from "../types/pressaoArterial";
 export interface Consulta {
- id: number;
- pacienteId: number;
- pacienteNome: string;
- medicoId: number;
- medicoNome: string;
- especialidade: string;
- usuarioId: number; // ID do usuário que agendou a consulta
- data: string;
- horario: string;
- status: StatusConsulta;
- observacoes?: string;
- valor?: number;
+  id: number;
+  pacienteId: number;
+  pacienteNome: string;
+  medicoId: number;
+  medicoNome: string;
+  especialidade: string;
+  usuarioId: number; // ID do usuário que agendou a consulta
+  data: string;
+  horario: string;
+  status: StatusConsulta;
+  observacoes?: string;
+  valor?: number;
+  prioridade?: boolean;
+  emergencia?: boolean;
+  pressaoSistolica?: number;
+  pressaoDiastolica?: number;
+  classificacaoPA?: ClassificacaoPA;
 }
